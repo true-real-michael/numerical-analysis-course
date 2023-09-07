1ab9c97730add8d59a382dda69047d3e772206ff
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-09-08 00:00:59 +0300

    bisection approximation

---
+++ task1/approximation/bisection_approximation.py
@@ -0,0 +1,25 @@
+import sympy as sp
+from sympy.abc import x as x_sym
+
+from .approximation import Approximation
+
+
+class BisectionApproximation(Approximation):
+    def _step(self) -> sp.Number:
+        pass
+
+    def _solve(self):
+        left = self.left
+        right = self.right
+        f = self.function
+
+        while right - left > 2 * self.eps:
+            self._inc_steps()
+            mid = (left + right) / 2
+            if f.subs(x_sym, mid) * f.subs(x_sym, left) <= 0:
+                right = mid
+            else:
+                left = mid
+                
+        self._value = (left + right) / 2
+
