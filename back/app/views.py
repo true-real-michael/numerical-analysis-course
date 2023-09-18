90715ef79f13b1e6405a942cdbd2472d627a296d
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-09-18 23:06:40 +0300

    moving to a separate folder

--- back/app/views.py
+++ back/app/views.py
@@ -1,9 +1,28 @@
-from flask import Blueprint, flash
+from flask import Blueprint, render_template, request, flash
 from flask_cors import cross_origin
 
-import task1
+from back import task1
 
 views = Blueprint("views", __name__)
+
+
+@views.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+
+@views.route("/task1.html", methods=["GET", "POST"])
+def task1_handler():
+    function = request.form.get("function", "")
+    left_bound = request.form.get("left_bound", "")
+    right_bound = request.form.get("right_bound", "")
+
+    try:
+        result = task1.solve(function, left_bound, right_bound)
+    except Exception as e:
+        result = ""
+        flash(str(e))
+    return render_template("task1.html", submitted_text=result)
 
 
 @views.route(
