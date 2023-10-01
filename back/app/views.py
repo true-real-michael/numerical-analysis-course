c19037a29189362ef937b7ed5203ed65ece63966
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-01 16:44:37 +0300

    handle

--- back/app/views.py
+++ back/app/views.py
@@ -11,22 +11,22 @@ def home():
     return render_template("home.html")
 
 
-@views.route("/task1.html", methods=["GET", "POST"])
-def task1_handler():
-    function = request.form.get("function", "")
-    left_bound = request.form.get("left_bound", "")
-    right_bound = request.form.get("right_bound", "")
-
+@views.route(
+    "/task1_endpoint/<function>&<left_bound>&<right_bound>&<n_divisions>&<eps>"
+)
+@cross_origin()
+def task_1endpoint(function, left_bound, right_bound, n_divisions, eps):
     try:
-        result = task1.solve(function, left_bound, right_bound)
+        result = task1.solve(function, left_bound, right_bound, n_divisions, eps)
     except Exception as e:
-        result = ""
+        result = {"error": str(e)}
         flash(str(e))
-    return render_template("task1.html", submitted_text=result)
+
+    return result
 
 
 @views.route(
-    "/task1_endpoint/<function>&<left_bound>&<right_bound>&<n_divisions>&<eps>"
+    "/task2_endpoint/<function>&<left_bound>&<right_bound>&<n_divisions>&<eps>"
 )
 @cross_origin()
 def task_1endpoint(function, left_bound, right_bound, n_divisions, eps):
