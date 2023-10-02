cfa63f78d5dbb54c476928e3d39f03a72bdf8a1a
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-02 20:04:16 +0300

    remove inner function

--- back/task2/solver.py
+++ back/task2/solver.py
@@ -22,14 +22,11 @@ def solve(function: str, left: str, right: str, n: str, x_values: str, x: str):
     assert n < len(x_values)
     assert all(tuple(map(lambda xi: left <= xi <= right, x_values)))
 
-    def f(x: float):
-        return float(sp.N(function.subs(x_sym, x)))
-
     x_values = sorted(x_values, key=lambda xi: abs(xi - x))
     selected = x_values[: n + 1]
 
     res = {
-        "points": [[x, f(x)] for x in selected],
+        "points": [[xi, float(sp.N(function.subs(x_sym, xi)))] for xi in selected],
         "approximation_by_method": [
             NewtonApproximation(function, selected, x).to_dict(),
             LagrangeApproximation(function, selected, x).to_dict(),
