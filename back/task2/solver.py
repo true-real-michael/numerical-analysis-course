274620eaffa7e41d6bc4f0decb90d32fb935c765
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-02 19:58:48 +0300

    add points to return

--- back/task2/solver.py
+++ back/task2/solver.py
@@ -1,6 +1,7 @@
 import json
 
 import sympy as sp
+from sympy.abc import x as x_sym
 
 from back.task2.approximation import NewtonApproximation
 from back.task2.approximation import LagrangeApproximation
@@ -21,12 +22,18 @@ def solve(function: str, left: str, right: str, n: str, x_values: str, x: str):
     assert n < len(x_values)
     assert all(tuple(map(lambda xi: left <= xi <= right, x_values)))
 
+    def f(x: float):
+        return float(sp.N(function.subs(x_sym, x)))
+
     x_values = sorted(x_values, key=lambda xi: abs(xi - x))
     selected = x_values[: n + 1]
 
-    res = [
-        NewtonApproximation(function, selected, x).to_dict(),
-        LagrangeApproximation(function, selected, x).to_dict(),
-    ]
+    res = {
+        "points": [[x, f(x)] for x in selected],
+        "approximation_by_method": [
+            NewtonApproximation(function, selected, x).to_dict(),
+            LagrangeApproximation(function, selected, x).to_dict(),
+        ],
+    }
 
     return json.dumps(res)
